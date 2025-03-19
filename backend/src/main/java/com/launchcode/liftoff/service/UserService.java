248eb07b55af8c74fa33820b169371c663e4ad13
strickte <166240135+strickte@users.noreply.github.com>
2025-03-18 23:19:40 -0500

    In theory mostly set up; need to work on commenting out and testing removal of 'role', but UserService throwing on all methods 'requires body instead of semicolon'

--- backend/src/main/java/com/launchcode/liftoff/service/UserService.java
+++ backend/src/main/java/com/launchcode/liftoff/service/UserService.java
@@ -1,3 +1,18 @@
+package com.launchcode.liftoff.service;
+
+import com.launchcode.liftoff.User;
+import java.util.List;
+
 public class UserService {
     
+    public List<User> getAllUser();
+     
+     public User findUserProfileByJwt(String jwt);
+     
+     public User findUserByEmail(String email);
+     
+     public User findUserById(String userId);
+
+     public List<User> findAllUsers();
+
 }
