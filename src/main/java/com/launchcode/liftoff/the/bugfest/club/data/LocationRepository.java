f28f612b9205ef7351ce5b66ef1037b7219942e1
Natalia Sirtak <nsirtakjob@gmail.com>
2025-03-20 14:29:17 -0500

    Revert "Feature/crud"

--- src/main/java/com/launchcode/liftoff/the/bugfest/club/data/LocationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.launchcode.liftoff.the.bugfest.club.data;
-
-import com.launchcode.liftoff.the.bugfest.club.models.Location;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LocationRepository extends CrudRepository<Location, Long> {
-}
