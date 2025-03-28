d5b0773026faf42e6ad8708459036268cd952935
strickte <166240135+strickte@users.noreply.github.com>
2025-03-28 18:34:12 -0500

    Throwing error inished with non-zero exit value 1

--- backend/src/main/java/com/launchcode/liftoff/the/bugfest/club/models/User.java
+++ backend/src/main/java/com/launchcode/liftoff/the/bugfest/club/models/User.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import jakarta.persistence.*;
 
-import org.springframework.data.annotation.Id;
+//import org.springframework.data.annotation.Id;
+import jakarta.persistence.Id;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -23,8 +24,8 @@ public class User {
 //    private final List<Destination> destinations = new ArrayList<>();
 
     @Id
-    @GeneratedValue
-    private String id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String fullName;
     private String email;
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
@@ -35,47 +36,47 @@ public class User {
 
 
 
-    public String getId() {
-        return id;
-    }
-    public void setId(String id) {
-        this.id = id;
-    }
-    public String get_id() {
-        return id;
-    }
-    public void set_id(String id) {
-        this.id = id;
-    }
-    public String getFullName() {
-        return fullName;
-    }
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public String getRole() {
-        return role;
-    }
-    public void setRole(String role) {
-        this.role = role;
-    }
-    public String getMobile() {
-        return mobile;
-    }
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
+//    public String getId() {
+//        return id;
+//    }
+//    public void setId(String id) {
+//        this.id = id;
+//    }
+//    public String get_id() {
+//        return id;
+//    }
+//    public void set_id(String id) {
+//        this.id = id;
+//    }
+//    public String getFullName() {
+//        return fullName;
+//    }
+//    public void setFullName(String fullName) {
+//        this.fullName = fullName;
+//    }
+//    public String getEmail() {
+//        return email;
+//    }
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//    public String getPassword() {
+//        return password;
+//    }
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//    public String getRole() {
+//        return role;
+//    }
+//    public void setRole(String role) {
+//        this.role = role;
+//    }
+//    public String getMobile() {
+//        return mobile;
+//    }
+//    public void setMobile(String mobile) {
+//        this.mobile = mobile;
+//    }
 
 }
