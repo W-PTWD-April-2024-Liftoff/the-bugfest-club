29c365dee925743f10d23a415a0b875b5c9a77b5
strickte <166240135+strickte@users.noreply.github.com>
2025-04-10 19:34:30 -0500

    Sych, third time is the charm to update routes

--- frontend-travel-vibes/src/App.jsx
+++ frontend-travel-vibes/src/App.jsx
@@ -1,6 +1,9 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Home';
-import CreateTripForm from './CreateTripForm';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import CreateTripForm from "./CreateTripForm";
+import LoginPage from "./components/LoginPage";
+import SignupPage from "./components/SignUpPage";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
@@ -9,9 +12,15 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create-trip" element={<CreateTripForm />} />
-          <Route path="/search" element={<div>Search Page (Coming Soon)</div>} />
+          <Route
+            path="/search"
+            element={<div>Search Page (Coming Soon)</div>}
+          />
           <Route path="/login" element={<div>Login Page (Coming Soon)</div>} />
           <Route path="/about" element={<div>About Page (Coming Soon)</div>} />
+          <Route path="/loginpage" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
           {/* Add a catch-all route for undefined paths */}
           <Route path="*" element={<div>404 - Page Not Found</div>} />
         </Routes>
